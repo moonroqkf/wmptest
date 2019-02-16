b22093a224faa3fc9af9c4cca078ead08585e287
Moon <Moon@Moon-PC>
2019-02-17 00:21:21 +0900

    vo 멤버변수에 대한 주석 추가

--- wmptest/src/main/java/com/wmptest/main/vo/MainVo.java
+++ wmptest/src/main/java/com/wmptest/main/vo/MainVo.java
@@ -2,13 +2,24 @@ package com.wmptest.main.vo;
 
 public class MainVo {
 	
+	/** 읽어들일 웹페이지 url */
 	String url;
+	
+	/** 가공한 html 텍스트 */
 	String html;
+	
+	/** 출력타입 구분 */
 	String type;
+	
+	/** 출력묶음 단위 */
 	int printGroupSize;
 	
+	/** 출력결과값(몫) */
 	String[] result;
+	
+	/** 출력결과값(나머지) */
 	String remainStr;
+	
 	public String getUrl() {
 		return url;
 	}
